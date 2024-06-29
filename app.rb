b6c73def18a665cf3f063b6be124bed1f526410e
nanwar0 <nina.anwar@chicagobooth.edu>
2024-06-29 22:24:50 +0000

    RPS pages with results

--- app.rb
+++ app.rb
@@ -7,3 +7,18 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  @opponent = rand(1..3)
+  erb(:rock)
+end
+
+get("/paper") do
+  @opponent = rand(1..3)
+  erb(:paper)
+end
+
+get("/scissors") do
+  @opponent = rand(1..3)
+  erb(:scissors)
+end
